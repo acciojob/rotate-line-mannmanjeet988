84605d662aee51ac8dcb3a10434ee46ffd75ea54
mannmanjeet988 <120735535+mannmanjeet988@users.noreply.github.com>
2023-06-21 14:43:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function rotateLine() {
+      const line = document.getElementById('line');
+      let rotation = 0;
+
+      setInterval(() => {
+        rotation += 1;
+        line.style.transform = `rotate(${rotation}deg)`;
+      }, 10);
+    }
+
+    // Call the rotateLine function
+    rotateLine();
